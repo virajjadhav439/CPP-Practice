89a3103f4186bd47f91fe4a0d3b43e1bf6382f41
virajjadhav439 <virajjadhav439@gmail.com>
2026-03-27 22:55:30 +0530

    Slight CHange

--- Practice/Sumitra-arora-python-in-CPP 11th/Chapter 4/9PrintSeries.cpp
+++ Practice/Sumitra-arora-python-in-CPP 11th/Chapter 4/9PrintSeries.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 using namespace std;
 
+// there are 2 approaches
+
 int main(){
     for (int i = 1; i < 41; i+=3)
     {
         // a part 
         cout<<i<<" ";
 
-        //b part
+        b part
         
-        // if (i%2==0)
-        // {
-        //     cout<<i*-1<<" ";
-        // }
-        // else{
-        //     cout<<i<<" ";
-        // }
+        if (i%2==0)
+        {
+            cout<<i*-1<<" ";
+        }
+        else{
+            cout<<i<<" ";
+        }
     }
     
     return 0;
